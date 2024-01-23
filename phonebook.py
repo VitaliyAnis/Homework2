2cb11bae37a35409bc21697f94230043490fecab
Виталий <vital228228123@gmail.com>
2024-01-23 19:07:34 +0600

    firstname

--- phonebook.py
+++ phonebook.py
@@ -11,9 +11,10 @@ def work_with_phonebook():
             last_name = input('Введите фаминию абонента: ')
             print(find_by_lastname(phone_book, last_name))
         elif choice == 3:
-            last_name = input('lastname ')
-            new_number = input('new  number ')
-            print(change_number(phone_book, last_name, new_number))
+            last_name = input('Введите фамилию абонента:')
+            new_number = input('Введите новый номер абонента: ')
+            change_number(phone_book, last_name, new_number)
+            write_txt('phon.txt', phone_book)
         elif choice == 4:
             lastname = input('lastname ')
             print(delete_by_lastname(phone_book, lastname))
@@ -67,13 +68,16 @@ def find_by_lastname(phone_book, last_name):
                     print(i)
 
 
-def change_number(phone_book, last_name, new_number):
-    with open('phon.txt', 'w', encoding='utf-8') as phout:
-        family = last_name
-        for i in phone_book:
-            for j in i.values():
-                if j == family:
-                    phout.write(f'{new_number[:-1]}\n')
+# def change_number(phone_book, last_name, new_number):
+#     with open('phon.txt', 'w', encoding='utf-8') as phout:
+#         family = last_name
+#         for i in phone_book:
+#             for j in i.values():
+#                 if j == family:
+#                     j = new_number
+#                     return j
+
+
 
 
 
